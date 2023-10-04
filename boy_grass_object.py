d7a5dc5a2669ddda2e8aa6834ff610f96cc57445
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-04 18:35:28 +0900

    Update boy_grass_object.py

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,13 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self): # 생성자.
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
+    def update(self):
+        pass
 
 def handle_events():
     global running
@@ -11,11 +18,28 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def reset_world():
+    global running
+    running = True
+
+def update_world():
+    pass
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world() # game logic
+    render_world() # draw game world
+    delay(0.05)
 
 # finalization code
 
